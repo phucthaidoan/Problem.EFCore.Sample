af39d52a320be9040a1575d7bfeda5274aa1554b
Phuc Thai <bindirt@gmail.com>
2024-03-24 13:41:54 +0700

    Add migraion to update auditable fields to be nullable

--- Data/Entities/Plan.cs
+++ Data/Entities/Plan.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
         public ICollection<Todo> Todos { get; set; }
     }
 }
--- Migrations/20240324064055_Change_Auditable_DateTime_To_Be_Nullable.Designer.cs
+++ Migrations/20240324064055_Change_Auditable_DateTime_To_Be_Nullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Problem.EFCore.Sample.Data;
 
@@ -11,9 +12,11 @@ using Problem.EFCore.Sample.Data;
 namespace Problem.EFCore.Sample.Migrations
 {
     [DbContext(typeof(TodoDbContext))]
-    partial class TodoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240324064055_Change_Auditable_DateTime_To_Be_Nullable")]
+    partial class Change_Auditable_DateTime_To_Be_Nullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
