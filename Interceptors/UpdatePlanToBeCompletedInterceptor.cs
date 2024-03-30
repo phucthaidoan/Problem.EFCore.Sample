7dcfb6cd1355e8e557aabaa5aabf96dc57b4e7b4
Phuc Thai <bindirt@gmail.com>
2024-03-30 11:33:20 +0700

    Change to use dbcontext interceptor

---
+++ Interceptors/UpdatePlanToBeCompletedInterceptor.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Problem.EFCore.Sample.Data.Entities;
+using Problem.EFCore.Sample.Data;
+
+namespace Problem.EFCore.Sample.Interceptors
+{
+    public class UpdatePlanToBeCompletedInterceptor : SaveChangesInterceptor
+    {
+        public override async ValueTask<int> SavedChangesAsync(
+            SaveChangesCompletedEventData eventData,
+            int result,
+            CancellationToken cancellationToken = default)
+        {
+            var todoDbContext = eventData.Context as TodoDbContext;
+            if (todoDbContext is not null)
+            {
+                // TODO if multiple todo entities are from the same plan => let's resolve it!
+                await HandleModifiedTodoEntitiesAsync(todoDbContext);
+            }
+
+            return await base.SavedChangesAsync(eventData, result, cancellationToken);
+        }
+
+        private async Task HandleModifiedTodoEntitiesAsync(TodoDbContext todoDbContext)
+        {
+            var modifiedTodoEntries = todoDbContext
+                .ChangeTracker
+                .Entries<Todo>()
+                .Where(todoEntry => todoEntry.State == EntityState.Modified)
+                .ToList();
+
+            foreach (var entry in modifiedTodoEntries)
+            {
+                var todoEntity = entry.Entity;
+                var isOriginalTaskDone = false;
+                entry.OriginalValues.TryGetValue(nameof(Todo.IsDone), out isOriginalTaskDone);
+
+                var isCurrentTaskDone = false;
+                entry.CurrentValues.TryGetValue(nameof(Todo.IsDone), out isCurrentTaskDone);
+
+                if (isOriginalTaskDone != isCurrentTaskDone)
+                {
+                    todoEntity.CompletedDate = isCurrentTaskDone ? DateTime.Now : null;
+
+                    await CheckToCompletePlanAsync(todoDbContext, todoEntity);
+                }
+            }
+        }
+
+        private async Task CheckToCompletePlanAsync(TodoDbContext todoDbContext, Todo todo)
+        {
+
+            var planData = await todoDbContext
+                .Plans
+                .Where(plan => plan.Id == todo.PlanId)
+                .Select(plan => new
+                {
+                    Plan = plan,
+                    HasTodoItemNotDone = plan
+                        .Todos
+                        .Where(todo => todo.Id != todo.Id)
+                        .Any(todo => !todo.IsDone)
+                })
+                .FirstOrDefaultAsync();
+
+            if (planData is null)
+            {
+                return;
+            }
+
+            var isAllItemsDone = todo.IsDone && !planData.HasTodoItemNotDone;
+            if (!isAllItemsDone)
+            {
+                planData.Plan.UpdatedDate = DateTime.Now;
+                planData.Plan.CompletedDate = null;
+            }
+            else
+            {
+                planData.Plan.UpdatedDate = DateTime.Now;
+                planData.Plan.CompletedDate = DateTime.Now;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
 using Problem.EFCore.Sample.Data;
+using Problem.EFCore.Sample.Interceptors;
 
 namespace Problem.EFCore.Sample
 {
@@ -15,7 +17,14 @@ namespace Problem.EFCore.Sample
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<TodoDbContext>();
+
+            builder.Services.AddSingleton<UpdatePlanToBeCompletedInterceptor>();
+            builder.Services.AddDbContext<TodoDbContext>((sp, options) =>
+            {
+                options
+                .UseSqlServer("Server=LAPTOP-IAJ1J0A2;Database=todo_01simple;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true")
+                .AddInterceptors(sp.GetRequiredService<UpdatePlanToBeCompletedInterceptor>());
+            });
             builder.Services.AddScoped<IPlanService, PlanService>();
             builder.Services.AddScoped<ITodoService, TodoService>();
             var app = builder.Build();
